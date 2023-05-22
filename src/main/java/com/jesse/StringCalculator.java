452c9bf9c2a8b0fb947e570710236d9328040251
Jesse Jiang <hao.jiang@kiwiplan.com>
2023-05-22 18:58:07 +1200

    step 4: custom delimiter

--- src/main/java/com/jesse/StringCalculator.java
+++ src/main/java/com/jesse/StringCalculator.java
@@ -8,6 +8,7 @@ package com.jesse;
 
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.Optional;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -16,10 +17,19 @@ import org.apache.logging.log4j.Logger;
  * A Calculator utility class
  */
 public class StringCalculator {
+  /**
+   * Default delimiter
+   */
   private static final String DEFAULT_DELIMITER = ",";
 
+  /**
+   * New Line
+   */
   private static final String NEWLINE = "\n";
 
+  /**
+   * Regex to check if we have , followed by \n or \n followed by ,
+   */
   private static final String CHECK_INPUT = ".*(\n,|,\n).*";
 
   private final Logger logger = LogManager.getLogger(StringCalculator.class);
@@ -31,7 +41,14 @@ public class StringCalculator {
     if (numbers.matches(CHECK_INPUT)) {
       throw new IllegalArgumentException("one or the other delimiter only, but not two delimiter come together.");
     }
-    String sanitizedNumberStr = numbers.replace(NEWLINE, ",").strip();
+    Optional<String> delimiter = getDelimiter(numbers);
+    String sanitizedNumberStr;
+    if (delimiter.isPresent()) {
+      sanitizedNumberStr = numbers.substring(numbers.indexOf(NEWLINE)+1).replace(NEWLINE, ",").replace(delimiter.get(), ",").strip();
+    }
+    else {
+      sanitizedNumberStr = numbers.replace(NEWLINE, ",").strip();
+    }
 
     return Arrays.stream(sanitizedNumberStr.split(DEFAULT_DELIMITER))
                  .filter(Objects::nonNull)
@@ -46,5 +63,17 @@ public class StringCalculator {
                    return number;
                  })
                  .reduce(0, Integer::sum);
+  }
+
+  private Optional<String> getDelimiter(String numbers) {
+    if (null == numbers || !numbers.contains(NEWLINE)) {
+      return Optional.empty();
+    }
+    String[] split = numbers.strip().split(NEWLINE);
+    String delimiterLine = split[0];
+    if (delimiterLine.startsWith("//") && delimiterLine.length() > 2) {
+      return Optional.of(delimiterLine.substring(2));
+    }
+    return Optional.empty();
   }
 }
--- src/test/java/com/jesse/TestStringCalculator.java
+++ src/test/java/com/jesse/TestStringCalculator.java
@@ -37,12 +37,23 @@ class TestStringCalculator {
     assertEquals(4, stringCalculator.add("0,2,2"));
     assertEquals(4, stringCalculator.add("0,2,2,a,b,c"));
   }
+
   @Test
-  void testNewLine(){
+  void testNewLine() {
     assertEquals(6, stringCalculator.add("1\n2,3"));
     assertEquals(6, stringCalculator.add("1\n\n2,3"));
     assertEquals(6, stringCalculator.add("1\n\n2,,3"));
-    assertThrows(IllegalArgumentException.class,()->stringCalculator.add("1,\n2"));
-    assertThrows(IllegalArgumentException.class,()->stringCalculator.add("1\n,3"));
+    assertThrows(IllegalArgumentException.class, () -> stringCalculator.add("1,\n2"));
+    assertThrows(IllegalArgumentException.class, () -> stringCalculator.add("1\n,3"));
+  }
+  @Test
+  void testCustomDelimiter(){
+    // we suppose the delimiter line is start with // and end with new line \n
+    assertEquals(3,stringCalculator.add("//;\n1;2"));
+    assertEquals(3,stringCalculator.add("//;\n1,2"));
+    assertEquals(3,stringCalculator.add("//;\n1\n2"));
+    assertEquals(0,stringCalculator.add(";\n1;2"));
+    assertEquals(3,stringCalculator.add(";\n1\n2"));
+    assertEquals(3,stringCalculator.add(";\n1,2"));
   }
 }
